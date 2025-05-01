7d774f5b216091771c9d9fe2eaab6b4ca2d773d7
porant <porant@gmail.com>
2025-05-01 02:08:02 +0200

    modified:   config.js 	modified:   docker-compose.yml

--- config.js
+++ config.js
@@ -1,14 +1,15 @@
 module.exports = {
-    windowTitle: "Porant IRC",
-    theme: "Dark",
-    restricted: true,
-    startupOptions: {
-      server: "irc.porant.de",
-      port: 6667,
-      tls: false,
-      channel: "#mychannel",
-      nick: "Guest%%%"
-    },
-    hideSettings: true
-  };
+  windowTitle: "Porant IRC",
+  theme: "Dark",
+  restricted: true,
+  startupOptions: {
+    server:    "irc.porant.de",
+    port:      6667,
+    tls:       false,
+    channel:   "#mychannel",
+    nick:      "Guest%%%"
+  },
+  hideSettings: true
+};
+
   
